14cfe7b3cf320fd03d7f6a72efc8965a9fdf2756
skulito <fox04012007@gmail.com>
2021-01-30 13:46:27 -0300

    Enter

---
+++ calorias.js
@@ -0,0 +1,32 @@
+const exercicio = {
+
+    txtempo: document.querySelector('#tempo'),
+    txtpeso: document.querySelector('#peso'),
+    txtvel: document.querySelector('#vel') 
+
+}
+
+function calcular() {
+
+    let arrayS = [exercicio.txtempo.value, exercicio.txtpeso.value, exercicio.txtvel.value]
+    let arrayN = [Number(exercicio.txtempo.value)/60, Number(exercicio.txtpeso.value), Number(exercicio.txtvel.value)]
+
+    if (arrayS[0, 1, 2] == 0) {
+
+        alert('[ERRO] Nenhuma Caloria Consumida')
+
+    } else {
+
+        let total = 1
+
+        for (let v in arrayN) {
+
+            total *= arrayN[v]
+
+        }
+
+        alert(total + ' - Calorias Consumidas')
+
+    }
+
+}
