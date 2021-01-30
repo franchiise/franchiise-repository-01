f6d82947206cfba16e4b274052e7bf3ba8955413
skulito <fox04012007@gmail.com>
2021-01-30 15:51:37 -0300

    Enter

--- kcal/calorias.js
+++ kcal/calorias.js
@@ -1,6 +1,30 @@
 /*
 
+    Para catalogar cada tipo de dado, pôr cada valor como
+    object seria a melhor opção. A partir da function cal-
+    cular(), armazenamos cada valor da const exercicio, e 
+    convertemos para numbers. 
 
+    Por meio de pesquisas, para calcular a quantidade de
+    calorias consumidas em, no caso, um exercício, existe
+    uma maneiro um tanto quanto simples, de determinar 
+    a quantidade apróximada de calorias perdidas.
+
+    O cálculo se torna o produto entre: O tempo percorrido;
+    o peso de quem se identifica; e a velocidade com a qual
+    se deslocou. O resultado, como dito anteriormente, não
+    pode ser exato, pois sempre se leva em consideração
+    diversos fatores como altura, gênero, biotipo corporal,
+    etc. 
+
+    Para formar um meio de multiplicar os elementos do object,
+    e que agora se tornou um array, cria-se um loop, que multi-
+    plicará cada valor apresentado na interface do html.
+
+    Fontes:
+
+    > https://www.tuasaude.com/gasto-calorico-dos-exercicios/
+    > https://www.inovenutrition.com.br/aprenda-calcular-o-gasto-calorico/
 
 */
 
